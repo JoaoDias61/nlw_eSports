094fd33ce95798a5fcaa85d297a1881b796caf5a
JoaoDias61 <jpdias641@gmail.com>
2023-01-02 22:33:27 -0300

    draft: routes using http methods

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,25 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', () => {
+app.get("/games", (request, response) => {
+    return response.json([])
+});
+
+app.post("/ads", (request, response) => {
+    return response.json([])
+});
+
+
+app.get('/games/:id/ads', (request, response) => {
+    const gameId = request.params.id;
+
     console.log("acessou ads")
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    const gameId = request.params.id;
+
+    return response.json([])
 })
 
 app.listen(3333)
